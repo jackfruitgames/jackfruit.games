90e94075d71df8e3140f2213b054e26c0f70e5dd
eddex <eddex@outlook.com>
2020-11-06 16:31:09 +0100

    feat: update DeathRollSimulator page layout

--- src/components/DeathRollSimulator.js
+++ src/components/DeathRollSimulator.js
@@ -1,9 +1,21 @@
+import './DeathRollSimulator.css'
+import { Container, Row, Col } from 'reactstrap'
+
 const DeathRollSimulator = () => {
     return (
-        <div>
-            <h1>WoW Death Roll Simulator</h1>
-            <p>Some decription.</p>
-        </div>
+        <Container>
+            <Row>
+                <Col>
+                <h1>WoW Death Roll Simulator</h1>
+                <span>Released: 05. November 2020</span>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <p className="big-text">TODO</p>
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
