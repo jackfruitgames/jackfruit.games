ccccd91df1d0b62a125e914748a58bbfec53c019
eddex <eddex@outlook.com>
2020-11-06 16:33:43 +0100

    feat: add navbar item for DeathRollSimulator

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,30 @@
+import React, { useState } from 'react';
 import {
+    Collapse,
+    Nav,
     Navbar,
-    NavbarBrand
+    NavbarBrand,
+    NavItem,
+    NavLink,
+    NavbarToggler
   } from 'reactstrap';
 
-const Navigation = () => {
+const Navigation = (props) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle = () => setIsOpen(!isOpen);
+
     return (
         <div>
             <Navbar className="App-Navbar" dark expand="md">
                 <NavbarBrand href="/">Jackfruit Games</NavbarBrand>
+                <NavbarToggler onClick={toggle} />
+                <Collapse isOpen={isOpen} navbar>
+                    <Nav className="mr-auto" navbar>
+                        <NavItem>
+                            <NavLink href="/DeathRollSimulator">WoW Death Roll Simulator</NavLink>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
             </Navbar>
         </div>
     );
